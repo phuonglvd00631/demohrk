fbfda8a59b31d28224af75a6a8d9c3999fd7c9f2
Hoàng Trung Kiên <phuonglvd00631@gmail.com>
2021-10-18 09:53:11 +0700

    init project

---
+++ src/main/java/com/example/demohrk/DemoHrkApplication.java
@@ -0,0 +1,13 @@
+package com.example.demohrk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHrkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoHrkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demohrk/DemoHrkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demohrk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHrkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
